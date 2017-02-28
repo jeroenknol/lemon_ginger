c14632ad70e1575de2401b96bf2084a7c2c4ff2d
Wouter de Vos <wouter@codaisseur.com>
2017-02-28 16:43:32 +0100

    Add FB login button

---
+++ src/screens/Login.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import { LoginButton, AccessToken } from 'react-native-fbsdk'
+import { View } from 'react-native';
+
+class Login extends Component {
+  render() {
+    return (
+      <View>
+        <LoginButton
+          publishPermissions={["publish_actions"]}
+          onLoginFinished={
+            (error, result) => {
+              if (error) {
+                alert("login has error: " + result.error);
+              } else if (result.isCancelled) {
+                alert("login is cancelled.");
+              } else {
+                AccessToken.getCurrentAccessToken().then(
+                  (data) => {
+                    alert(data.accessToken.toString())
+                  }
+                )
+              }
+            }
+          }
+          onLogoutFinished={() => alert("logout.")}/>
+      </View>
+    );
+  }
+}
+
+export default Login
