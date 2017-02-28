e988afba8adbc48562755ab992a6f72f703ed460
Wouter de Vos <wouter@codaisseur.com>
2017-02-28 20:36:28 +0100

    Process FB login and get user data

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,13 +1,36 @@
 import React, { Component } from 'react'
-import { LoginButton, AccessToken } from 'react-native-fbsdk'
+import { LoginButton, AccessToken, LoginManager } from 'react-native-fbsdk'
 import { View } from 'react-native';
+import FBSDK, { GraphRequest, GraphRequestManager } from 'react-native-fbsdk'
 
 class Login extends Component {
+  logUser(error: ?Object, result: ?Object) {
+    if (error) {
+      console.error('Error fetching data: ' + JSON.stringify(error))
+    } else {
+      // result.id contains a unique user id for our FB app
+      // it *might* also include an email address...
+      console.log('Success fetching data: ' + JSON.stringify(result))
+    }
+  }
+
+  getUser() {
+    const { accessToken } = this
+    console.log(accessToken)
+    const infoRequest = new GraphRequest(
+      '/me',
+      { accessToken, version: "v2.8" },
+      this.logUser
+    )
+
+    // Start the graph request.
+    new GraphRequestManager().addRequest(infoRequest).start()
+  }
   render() {
     return (
       <View>
         <LoginButton
-          publishPermissions={["publish_actions"]}
+          readPermissions={["public_profile", "email", "user_friends"]}
           onLoginFinished={
             (error, result) => {
               if (error) {
@@ -15,9 +38,15 @@ class Login extends Component {
               } else if (result.isCancelled) {
                 alert("login is cancelled.");
               } else {
+                const getUser = this.getUser.bind(this)
+
                 AccessToken.getCurrentAccessToken().then(
                   (data) => {
-                    alert(data.accessToken.toString())
+                    console.log(result)
+                    console.log(data)
+                    // alert(data.accessToken.toString())
+                    this.accessToken = data.accessToken
+                    getUser()
                   }
                 )
               }
