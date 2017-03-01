00f9ab328c29c4d8b44a37bfea1b69e257c1a5a2
jeroenknol <jeroen-knol@live.nl>
2017-03-01 20:01:06 +0100

    Add routes with dummy pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import React, { Component } from 'react';
-import { AppRegistry } from 'react-native';
+import { AppRegistry, Navigator, Text, TouchableHighlight } from 'react-native';
 
-import MyScene from './screens/MyScene';
+import Page1 from './screens/routing/Page1'
+import Page2 from './screens/routing/Page2'
+import Page3 from './screens/routing/Page3'
 
 class App extends Component {
+  renderScene(route, navigator) {
+    if (route.index === 0) {
+      return <Page1 navigator={navigator} />
+    } else if (route.index === 1) {
+      return <Page2 navigator={navigator} />
+    } else if (route.index === 2) {
+      return <Page3 navigator={navigator} />
+    }
+  }
+
   render() {
+    const routes = [
+      { name: 'pageone', title: 'Page One', index: 0, leftButton: null, rightButton: null, component: Page1 },
+      { name: 'pagetwo', title: 'Page Two', index: 1, leftButton: null, rightButton: null, component: Page2 },
+      { name: 'pagethree', title: 'Page Three', index: 2, leftButton: null, rightButton: null, component: Page3 },
+    ];
+
+
     return (
-      <MyScene />
+      <Navigator
+        initialRoute={routes[0]}
+        renderScene={this.renderScene.bind(this)}
+      />
     )
   }
 }
