606a37fcd6355c1babb8cb49e32f0fd60e9ddce3
timkuilman <tim.kuilman@gmail.com>
2017-03-02 18:49:40 +0100

    Recipes comes in with the fetch action

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { Text, View, Button } from 'react-native';
+import { connect } from 'react-redux'                        // import connect
+import fetchRecipes from '../actions/recipes/fetch'          // import fetch recipes action
 import Login from './Login'
 import styles from './Home.styles'
 
@@ -8,8 +10,14 @@ class Home extends Component {
     console.log(this.props.navigator)
     this.props.navigator.push({index})
   }
+  componentWillMount() {
+    console.log("In componentWillMount, let's fire up fetchRecipes")
+    this.props.fetchRecipes()
+  }
+
 
   render() {
+    console.log('This propz0rs',this.props)
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
@@ -34,5 +42,5 @@ class Home extends Component {
   }
 }
 
-export default Home
-// connect(null, { navigateTo })(Home)
+
+export default connect(null, { fetchRecipes })(Home)      // fetchRecipes available as props
