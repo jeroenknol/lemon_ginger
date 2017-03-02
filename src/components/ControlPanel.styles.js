abb981c58809ff2e7268330edeebbbbe9cb597bf
jeroenknol <jeroen-knol@live.nl>
2017-03-02 18:45:23 +0100

    Added styling for menu

---
+++ src/components/ControlPanel.styles.js
@@ -0,0 +1,35 @@
+import { StyleSheet } from 'react-native'
+
+module.exports = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'space-between',
+    backgroundColor: '#4A4A4A',
+    opacity: 0.95,
+    marginTop: 20,
+  },
+  title: {
+    flex: 1,
+    textAlign: 'center',
+    fontSize: 32,
+    fontFamily: 'simonetta',
+    color: '#FFF',
+    marginTop: 20,
+    marginBottom: 200,
+  },
+  menuitems: {
+    flex: 1,
+    fontSize: 16,
+    textAlign: 'left',
+    marginLeft: 25,
+    color: "#FFF"
+  },
+  logout: {
+    flex: 1,
+    fontSize: 16,
+    textAlign: 'left',
+    marginLeft: 25,
+    marginTop: 200,
+    color: "#FFF"
+  }
+});
