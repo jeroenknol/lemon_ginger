5652850f5f1db2ba643c3dfacf9d302d474c71e4
timkuilman <tim.kuilman@gmail.com>
2017-03-02 20:01:31 +0100

    AllRecipes rendered from store

--- src/screens/AllRecipes.js
+++ src/screens/AllRecipes.js
@@ -1,24 +1,30 @@
 import React, { Component } from 'react';
-import { Text, ScrollView } from 'react-native';
+import { Text, ScrollView, View } from 'react-native';
+import { connect } from 'react-redux';
 import styles from './AllRecipes.styles'
 import RecipeItem from '../components/shared/RecipeItem'
 import Filter from '../components/allrecipes/Filter'
 
 class AllRecipes extends Component {
+
+  renderRecipe(recipe, index) {
+    return <RecipeItem key={ index } { ...recipe } />
+  }
+
   render() {
+    console.log('Props in allrecipesscreen:', this.props)
     return (
       <ScrollView style={styles.container}>
         <Filter />
-        <RecipeItem />
-        <RecipeItem />
-        <RecipeItem />
-        <RecipeItem />
-        <RecipeItem />
-        <RecipeItem />
+        <View>
+          { this.props.recipes.map(this.renderRecipe.bind(this)) }
+        </View>
+
       </ScrollView>
     );
   }
 }
 
-export default AllRecipes
-// connect(null, { navigateTo })(Home)
+const mapStateToProps = ({ recipes }) => ({ recipes })
+
+export default connect(mapStateToProps)(AllRecipes)
