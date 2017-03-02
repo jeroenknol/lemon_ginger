b20649aedbcdf27e579422084f04a72e7216c1cc
jeroenknol <jeroen-knol@live.nl>
2017-03-02 14:15:35 +0100

    Routing mess - for Wouter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import React, { Component } from 'react';
-import { AppRegistry, Navigator, Text, TouchableHighlight } from 'react-native';
+import { AppRegistry, Navigator, Text, TouchableHighlight, Button } from 'react-native';
 import styles from './App.styles'
+import Drawer from 'react-native-drawer'
+import ControlPanel from './components/ControlPanel'
 
 import Page1 from './screens/routing/Page1'
 import Page2 from './screens/routing/Page2'
 import Page3 from './screens/routing/Page3'
 
-class App extends Component {
+class Content extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      nextRoute: props.nextRoute || 0
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    console.log(nextProps)
+    const currentRoute = this.state.nextRoute
+    const nextRoute = nextProps.nextRoute
+
+    if (currentRoute != nextRoute) {
+      this.setState({
+        nextRoute
+      })
+    }
+  }
+
   renderScene(route, navigator) {
     if (route.index === 0) {
       return <Page1 navigator={navigator} />
@@ -46,5 +67,49 @@ class App extends Component {
     )
   }
 }
+
+
+class App extends Component {
+  constructor(props) {
+    super()
+    this.state = {
+      nextRoute: 0
+    }
+  }
+
+  // Drawer opening and closing functions - See https://github.com/root-two/react-native-drawer
+  closeControlPanel() {
+    this._drawer.close()
+  }
+
+  openControlPanel() {
+    this._drawer.open()
+  }
+
+  navigate(nextRoute) {
+    this.setState({
+      nextRoute
+    })
+  }
+
+  render() {
+    return(
+      <Drawer
+        ref={(ref) => this._drawer = ref}
+        content={<ControlPanel onNavigate={this.navigate.bind(this)} />}
+        tapToClose={true}
+        panOpenMask={100}
+        panCloseMask={100}
+        type={'overlay'}
+        open={false}
+        openDrawerOffset={100}
+        side={'right'}
+        >
+        <Content nextRoute={this.state.nextRoute} />
+      </Drawer>
+    )
+  }
+}
+
 
 export default App
