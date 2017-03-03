891ce19834a40db3d98f7554b7448f95067a0520
jeroenknol <jeroen-knol@live.nl>
2017-03-03 14:40:16 +0100

    Some styling for all recipes page

--- src/components/shared/RecipeItem.styles.js
+++ src/components/shared/RecipeItem.styles.js
@@ -2,7 +2,7 @@ import { StyleSheet } from 'react-native';
 
 module.exports = StyleSheet.create({
   recipeitem: {
-    height: 200,
+    height: 300,
     backgroundColor: '#F8F8F8',
     marginBottom: 6,
   },
@@ -10,21 +10,22 @@ module.exports = StyleSheet.create({
   thumb: {
     borderRadius: 3,
     width: 375,
-    height: 200,
+    height: 300,
   },
   text: {
-    fontFamily: 'Avenir Next',
-    fontSize: 18,
+    fontFamily: 'simonetta',
+    fontSize: 26,
     borderRadius: 3,
     flex: 1,
     marginTop: -50,
     minHeight: 50,
     padding: 8,
     color: '#fff',
-    backgroundColor: 'rgba(0,0,0,0.2)',
+    backgroundColor: 'rgba(0,0,0,0.4)',
   },
   buttons: {
-    marginTop: -190,
+    marginTop: -290,
+    marginRight: 10,
     justifyContent: 'flex-end',
     flexDirection: 'row'
 
